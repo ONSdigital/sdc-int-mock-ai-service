050e8b83768c5cec101ca0c08b4302581f3a729a
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2021-06-16 11:35:57 +0100

    SOC-20 Return http 404 when data not found

--- src/main/java/uk/gov/ons/ctp/integration/mockai/endpoint/AddressesEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/integration/mockai/endpoint/AddressesEndpoint.java
@@ -1,5 +1,7 @@
 package uk.gov.ons.ctp.integration.mockai.endpoint;
 
+import com.godaddy.logging.Logger;
+import com.godaddy.logging.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,19 +10,14 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-
 import javax.validation.Valid;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.godaddy.logging.Logger;
-import com.godaddy.logging.LoggerFactory;
-
 import uk.gov.ons.ctp.common.endpoint.CTPEndpoint;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.integration.mockai.misc.Constants;
@@ -52,7 +49,7 @@ public final class AddressesEndpoint implements CTPEndpoint {
   }
 
   @RequestMapping(value = "/addresses/rh/postcode/{postcode}", method = RequestMethod.GET)
-  public String getAddressesRhPostcode(
+  public ResponseEntity<String> getAddressesRhPostcode(
       @PathVariable(value = "postcode") String postcode,
       @Valid AddressesRhPostcodeRequestDTO requestParamsDTO)
       throws IOException, CTPException {
@@ -61,66 +58,69 @@ public final class AddressesEndpoint implements CTPEndpoint {
     log.with("postcode", postcode).info("Request " + requestType.getPath() + "/" + postcode);
 
     postcode = postcode.replaceAll(" ", "").trim();
-    String response = simulateAIResponse(requestType, postcode);
+    ResponseEntity<String> response = simulateAIResponse(requestType, postcode);
 
     return response;
   }
 
   @RequestMapping(value = "/addresses/partial", method = RequestMethod.GET)
-  public String getAddressesPartial(@RequestParam(required = true) String input)
+  public ResponseEntity<String> getAddressesPartial(@RequestParam(required = true) String input)
       throws IOException, CTPException {
     RequestType requestType = RequestType.AI_PARTIAL;
 
     log.with("input", input).info("Request " + requestType.getPath());
 
     String cleanedInput = input.replaceAll(" ", "-").trim();
-    String response = simulateAIResponse(requestType, cleanedInput);
+    ResponseEntity<String> response = simulateAIResponse(requestType, cleanedInput);
 
     return response;
   }
 
   @RequestMapping(value = "/addresses/postcode/{postcode}", method = RequestMethod.GET)
-  public String getAddressesPostcode(@PathVariable(value = "postcode") String postcode)
-      throws IOException, CTPException {
+  public ResponseEntity<String> getAddressesPostcode(
+      @PathVariable(value = "postcode") String postcode) throws IOException, CTPException {
     RequestType requestType = RequestType.AI_POSTCODE;
 
     log.with("postcode", postcode).info("Request " + requestType.getPath() + "/" + postcode);
 
     postcode = postcode.replaceAll(" ", "").trim();
-    String response = simulateAIResponse(requestType, postcode);
+    ResponseEntity<String> response = simulateAIResponse(requestType, postcode);
 
     return response;
   }
 
   @RequestMapping(value = "/addresses/rh/uprn/{uprn}", method = RequestMethod.GET)
-  public String getRhPostcode(@PathVariable(value = "uprn") String uprn)
+  public ResponseEntity<String> getRhPostcode(@PathVariable(value = "uprn") String uprn)
       throws IOException, CTPException {
     RequestType requestType = RequestType.AI_RH_UPRN;
 
     log.with("uprn", uprn).info("Request " + requestType.getPath() + "/" + uprn);
 
     uprn = uprn.replaceAll(" ", "").trim();
-    String response = simulateAIResponse(requestType, uprn);
+    ResponseEntity<String> response = simulateAIResponse(requestType, uprn);
 
     return response;
   }
 
-  private String simulateAIResponse(RequestType requestType, String baseFileName)
+  private ResponseEntity<String> simulateAIResponse(RequestType requestType, String baseFileName)
       throws IOException, CTPException {
-    String response = readCapturedAiResponse(requestType, baseFileName);
+    HttpStatus responseStatus = HttpStatus.OK;
+    String responseText = readCapturedAiResponse(requestType, baseFileName);
 
-    if (response == null) {
-      response = readCapturedAiResponse(requestType, Constants.NO_DATA_FILE_NAME);
+    if (responseText == null) {
+      // 404 - not found
+      responseStatus = HttpStatus.NOT_FOUND;
+      responseText = readCapturedAiResponse(requestType, Constants.NO_DATA_FILE_NAME);
 
       // Replace any place holders with actual values
       String placeholderName = requestType.getPlaceholderName();
       if (placeholderName != null) {
         String fullPlaceholderName = "%" + placeholderName + "%";
-        response = response.replace(fullPlaceholderName, baseFileName);
+        responseText = responseText.replace(fullPlaceholderName, baseFileName);
       }
     }
 
-    return response;
+    return new ResponseEntity<String>(responseText, responseStatus);
   }
 
   private String readCapturedAiResponse(RequestType requestType, String baseFileName)
